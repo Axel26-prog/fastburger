030e6c49d2d3f4b136b7d8562c5a7cb4b4a3df8b
Axel <axelguzman996@gmail.com>
2026-06-21 14:55:02 -0600

    commit final avance 3

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -6,7 +6,7 @@ namespace FastBurger.Web.Controllers;
 public class MenuController : Controller
 {
     private readonly IMenuService _menuService;
-
+            
     public MenuController(IMenuService menuService)
     {
         _menuService = menuService;
@@ -15,13 +15,24 @@ public class MenuController : Controller
     public async Task<IActionResult> Index()
     {
         var menus = await _menuService.GetAllAsync();
-        return View(menus);
+        return View(menus.OrderByDescending(m => m.FechaInicio));
     }
 
     public async Task<IActionResult> Detalle(int id)
     {
         var menu = await _menuService.GetByIdAsync(id);
         if (menu == null) return NotFound();
+        return View(menu);
+    }
+
+    public async Task<IActionResult> Disponible()
+    {
+        var menu = await _menuService.GetDisponibleAsync();
+        if (menu == null)
+        {
+            ViewBag.Mensaje = "No hay ningún menú disponible en este momento.";
+            return View(null);
+        }
         return View(menu);
     }
 }
